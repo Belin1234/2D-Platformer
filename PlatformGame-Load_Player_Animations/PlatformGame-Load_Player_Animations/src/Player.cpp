d80bccea144266b482951c01e6aa66e14ea7ecaf
Belin1234 <canaldebelin1234@gmail.com>
2024-10-26 17:27:41 +0200

    Code to Github

---
+++ PlatformGame-Load_Player_Animations/PlatformGame-Load_Player_Animations/src/Player.cpp
@@ -0,0 +1,263 @@
+#include "Player.h"
+#include "Engine.h"
+#include "Textures.h"
+#include "Audio.h"
+#include "Input.h"
+#include "Render.h"
+#include "Scene.h"
+#include "Log.h"
+#include "Physics.h"
+
+Player::Player() : Entity(EntityType::PLAYER)
+{
+	name = "Player";
+}
+
+Player::~Player() {
+
+}
+
+bool Player::Awake() {
+
+	//L03: TODO 2: Initialize Player parameters
+	position = Vector2D(0, 0);
+	return true;
+}
+
+bool Player::Start() {
+
+	//L03: TODO 2: Initialize Player parameters
+	texture = Engine::GetInstance().textures.get()->Load(parameters.attribute("texture").as_string());
+	position.setX(parameters.attribute("x").as_int());
+	position.setY(parameters.attribute("y").as_int());
+	texW = parameters.attribute("w").as_int();
+	texH = parameters.attribute("h").as_int();
+
+	//Load animations
+	idle.LoadAnimations(parameters.child("animations").child("idle"));
+	idleL.LoadAnimations(parameters.child("animations").child("idleL"));
+	run.LoadAnimations(parameters.child("animations").child("run"));
+	runL.LoadAnimations(parameters.child("animations").child("runL"));
+	jump.LoadAnimations(parameters.child("animations").child("jump"));
+	jumpL.LoadAnimations(parameters.child("animations").child("jumpL"));
+	die.LoadAnimations(parameters.child("animations").child("die"));
+	dieL.LoadAnimations(parameters.child("animations").child("dieL"));
+	/*currentAnimation = &idle;*/
+
+	// L08 TODO 5: Add physics to the player - initialize physics body
+	pbody = Engine::GetInstance().physics.get()->CreateCircle((int)position.getX(), (int)position.getY(), texW / 2, bodyType::DYNAMIC);
+
+	// L08 TODO 6: Assign player class (using "this") to the listener of the pbody. This makes the Physics module to call the OnCollision method
+	pbody->listener = this;
+
+	// L08 TODO 7: Assign collider type
+	pbody->ctype = ColliderType::PLAYER;
+
+	//initialize audio effect
+	pickCoinFxId = Engine::GetInstance().audio.get()->LoadFx("Assets/Audio/Fx/retro-video-game-coin-pickup-38299.ogg");
+
+	// God Mode desactivated first
+	godmode = false;
+	
+	// Starts looking to the right
+	lookRight = true;
+
+	return true;
+}
+
+bool Player::Update(float dt)
+{
+	// L08 TODO 5: Add physics to the player - updated player position using physics
+	b2Vec2 velocity;
+
+	if (!godmode) {
+		
+		velocity = b2Vec2(0, pbody->body->GetLinearVelocity().y);
+	}
+	
+
+	if (lookRight) {
+
+		currentAnimation = &idle;
+	}
+
+	if(!lookRight) {
+
+		currentAnimation = &idleL;
+	}
+	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// Quiero que al acabar de hacer el loop pero solo uno, respawnee en estado de idle
+	// Pero si pones false en el loop de config lo hace pero se queda el ultimo frame si lo vuelves a repetir cunado respawneas
+	// Dies and respawns
+	if (Engine::GetInstance().input.get()->GetKey(SDL_SCANCODE_T) == KEY_REPEAT) {
+
+		if (lookRight) {
+
+			
+			currentAnimation = &die;
+			
+			
+			pbody->body->SetTransform(b2Vec2(PIXEL_TO_METERS(100), PIXEL_TO_METERS(450)), 0);
+		}
+		else if (!lookRight) {
+			
+			currentAnimation = &dieL;
+
+			pbody->body->SetTransform(b2Vec2(PIXEL_TO_METERS(100), PIXEL_TO_METERS(450)), 0);
+		}
+	}
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	// Move left
+	if (Engine::GetInstance().input.get()->GetKey(SDL_SCANCODE_A) == KEY_REPEAT) {
+		velocity.x = -0.15 * dt;
+
+		lookRight = false;
+
+		if (!lookRight) {
+
+			currentAnimation = &runL;
+		}
+		
+	}
+	
+	// Move right
+	if (Engine::GetInstance().input.get()->GetKey(SDL_SCANCODE_D) == KEY_REPEAT) {
+		velocity.x = 0.15 * dt;
+		
+		lookRight = true;
+
+		if (lookRight) {
+
+			currentAnimation = &run;
+		}
+		
+		
+	}
+	
+	
+
+	// Jump
+	if (Engine::GetInstance().input.get()->GetKey(SDL_SCANCODE_SPACE) == KEY_DOWN && isJumping == false && !godmode) {
+		// Apply an initial upward force
+		pbody->body->ApplyLinearImpulseToCenter(b2Vec2(0, -jumpForce), true);
+		isJumping = true;
+	}
+
+	// If the player is jumpling, we don't want to apply gravity, we use the current velocity prduced by the jump
+	if (isJumping == true && !godmode)
+	{
+		if (lookRight) {
+			currentAnimation = &jump;
+		}
+		else if (!lookRight) {
+			currentAnimation = &jumpL;
+		}
+
+		velocity.y = pbody->body->GetLinearVelocity().y;
+	}
+	
+	// Activate /Desactivate God Mode
+	if (Engine::GetInstance().input.get()->GetKey(SDL_SCANCODE_F10) == KEY_DOWN && !godmode) {
+		
+		godmode = true;
+		
+	}
+	else if (Engine::GetInstance().input.get()->GetKey(SDL_SCANCODE_F10) == KEY_DOWN && godmode) {
+		
+		godmode = false;
+		velocity = b2Vec2(0, pbody->body->GetLinearVelocity().y);
+	}
+	
+	// God Mode On
+	if (godmode == true) {
+		velocity = b2Vec2(0, 0);
+		
+		if (Engine::GetInstance().input.get()->GetKey(SDL_SCANCODE_A) == KEY_REPEAT) {
+			velocity.x = -0.2 * dt;
+		}
+		
+		if (Engine::GetInstance().input.get()->GetKey(SDL_SCANCODE_D) == KEY_REPEAT) {
+			velocity.x = 0.2 * dt;
+		}
+
+		if (Engine::GetInstance().input.get()->GetKey(SDL_SCANCODE_S) == KEY_REPEAT) {
+			velocity.y = 0.2 * dt;
+		}
+		if (Engine::GetInstance().input.get()->GetKey(SDL_SCANCODE_W) == KEY_REPEAT) {
+			velocity.y = -0.2 * dt;
+		}
+
+	
+	}
+
+
+	
+
+	// Apply the velocity to the player
+	pbody->body->SetLinearVelocity(velocity);
+
+	b2Transform pbodyPos = pbody->body->GetTransform();
+	position.setX(METERS_TO_PIXELS(pbodyPos.p.x) - texH / 2);
+	position.setY(METERS_TO_PIXELS(pbodyPos.p.y) - texH / 2);
+
+
+	// Respawn when player falls of the map
+	if (position.getY() > 768 && !godmode) {
+		
+		
+		pbody->body->SetTransform(b2Vec2(PIXEL_TO_METERS(100), PIXEL_TO_METERS(450)), 0);
+
+	}
+
+
+	Engine::GetInstance().render.get()->DrawTexture(texture, (int)position.getX(), (int)position.getY(), &currentAnimation->GetCurrentFrame());
+	currentAnimation->Update();
+	return true;
+}
+
+bool Player::CleanUp()
+{
+	LOG("Cleanup player");
+	Engine::GetInstance().textures.get()->UnLoad(texture);
+	return true;
+}
+
+// L08 TODO 6: Define OnCollision function for the player. 
+void Player::OnCollision(PhysBody* physA, PhysBody* physB) {
+	switch (physB->ctype)
+	{
+	case ColliderType::PLATFORM:
+		LOG("Collision PLATFORM");
+		//reset the jump flag when touching the ground
+		isJumping = false;
+		break;
+	case ColliderType::ITEM:
+		LOG("Collision ITEM");
+		break;
+	case ColliderType::UNKNOWN:
+		LOG("Collision UNKNOWN");
+		break;
+	default:
+		break;
+	}
+}
+
+void Player::OnCollisionEnd(PhysBody* physA, PhysBody* physB)
+{
+	switch (physB->ctype)
+	{
+	case ColliderType::PLATFORM:
+		LOG("End Collision PLATFORM");
+		break;
+	case ColliderType::ITEM:
+		LOG("End Collision ITEM");
+		Engine::GetInstance().audio.get()->PlayFx(pickCoinFxId);
+		break;
+	case ColliderType::UNKNOWN:
+		LOG("End Collision UNKNOWN");
+		break;
+	default:
+		break;
+	}
+}
---
+++ PlatformGame-Load_Player_Animations/PlatformGame-Load_Player_Animations/src/Scene.cpp
@@ -0,0 +1,128 @@
+#include "Engine.h"
+#include "Input.h"
+#include "Textures.h"
+#include "Audio.h"
+#include "Render.h"
+#include "Window.h"
+#include "Scene.h"
+#include "Log.h"
+#include "Entity.h"
+#include "EntityManager.h"
+#include "Player.h"
+#include "Map.h"
+#include "Item.h"
+
+Scene::Scene() : Module()
+{
+	name = "scene";
+	img = nullptr;
+}
+
+// Destructor
+Scene::~Scene()
+{}
+
+// Called before render is available
+bool Scene::Awake()
+{
+	LOG("Loading Scene");
+	bool ret = true;
+
+	//L04: TODO 3b: Instantiate the player using the entity manager
+	player = (Player*)Engine::GetInstance().entityManager->CreateEntity(EntityType::PLAYER);
+	player->SetParameters(configParameters.child("entities").child("player"));
+	
+	//L08 Create a new item using the entity manager and set the position to (200, 672) to test
+	Item* item = (Item*) Engine::GetInstance().entityManager->CreateEntity(EntityType::ITEM);
+	item->position = Vector2D(200, 672);
+
+	helpMenu = Engine::GetInstance().textures.get()->Load("Assets/Textures/HelpMenu.png");
+	help = false;
+
+	return ret;
+}
+
+// Called before the first frame
+bool Scene::Start()
+{
+	//L06 TODO 3: Call the function to load the map. 
+	Engine::GetInstance().map->Load(configParameters.child("map").attribute("path").as_string(), configParameters.child("map").attribute("name").as_string());
+	
+
+	return true;
+}
+
+// Called each loop iteration
+bool Scene::PreUpdate()
+{
+	return true;
+}
+
+// Called each loop iteration
+bool Scene::Update(float dt)
+{
+	//L03 TODO 3: Make the 
+	// era movement independent of framerate
+	float camSpeed = 1;
+
+	/*if(Engine::GetInstance().input.get()->GetKey(SDL_SCANCODE_UP) == KEY_REPEAT)
+		Engine::GetInstance().render.get()->camera.y -= ceil(camSpeed * dt);
+
+	if(Engine::GetInstance().input.get()->GetKey(SDL_SCANCODE_DOWN) == KEY_REPEAT)
+		Engine::GetInstance().render.get()->camera.y += ceil(camSpeed * dt);
+
+	if(Engine::GetInstance().input.get()->GetKey(SDL_SCANCODE_LEFT) == KEY_REPEAT)
+		Engine::GetInstance().render.get()->camera.x -= ceil(camSpeed * dt);
+
+	if(Engine::GetInstance().input.get()->GetKey(SDL_SCANCODE_RIGHT) == KEY_REPEAT)
+		Engine::GetInstance().render.get()->camera.x += ceil(camSpeed * dt);*/
+	
+	
+	Engine::GetInstance().render.get()->camera.x = (Engine::GetInstance().window->width/2 - player->position.getX());
+	
+
+	/*DrawText("Hola, raylib!", 190, 200, 20, DARKGRAY);*/
+	/////////////////////////////////////////////////////////////////////
+	// Peta por el &destRect pero no se como pasarle los parametros
+	if (Engine::GetInstance().input.get()->GetKey(SDL_SCANCODE_H) == KEY_DOWN) {
+		help = !help;
+
+	}
+	
+	if (help) {
+	
+
+		// Crea la forma del rectangulo donde estara la textura
+		SDL_Rect destRect = {
+			(Engine::GetInstance().render.get()->camera.x),  // Posición x
+			(Engine::GetInstance().render.get()->camera.y),  // Posición y
+			(Engine::GetInstance().window.get()->width),     // Ancho
+			(Engine::GetInstance().window.get()->height)     // Alto
+		};
+
+		Engine::GetInstance().render.get()->DrawTexture(helpMenu,0,0, &destRect, 0.0f, 0.0, 1,1);
+	}
+	//////////////////////////////////////////////////////////////////
+	return true;
+}
+
+// Called each loop iteration
+bool Scene::PostUpdate()
+{
+	bool ret = true;
+
+	if(Engine::GetInstance().input.get()->GetKey(SDL_SCANCODE_ESCAPE) == KEY_DOWN)
+		ret = false;
+
+	return ret;
+}
+
+// Called before quitting
+bool Scene::CleanUp()
+{
+	LOG("Freeing scene");
+
+	SDL_DestroyTexture(img);
+
+	return true;
+}
